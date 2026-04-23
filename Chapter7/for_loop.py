1385c1064f18194d9fb932e80f6d7a302cce494e
Shahan-Waheed728 <shahanwaheed43@gmail.com>
2026-04-23 10:23:59 +0500

    Chapter 6 and 7 updated

---
+++ Chapter7/for_loop.py
@@ -0,0 +1,94 @@
+# # python program to print your name 10 times
+# name = "Shahan Waheed"
+# for n in name:
+#     print(n)
+
+# for i in range(11):
+#     print("Shahan Waheed")
+
+# for i in range(1,16,3):
+#     print(i)
+# students = ["Shahan","Ali","Fahad","Talha","Niha","Zoya"]
+# for s in students:
+#     print(s)
+# else:
+#     print("Done...")
+
+# n = 5
+# for i in range(11):
+#     print(f"{n} * {i} = {n*i}")
+# python program to print table of 2
+# n = 2
+# for i in range(1,11):
+#     if(i == 5):
+#         break
+#     print(f"{n} * {i} = {n*i}")
+#     n = 2
+
+# python program to print table of 12
+# n = 12
+# for i in range(1,11):
+#     if(i == 5):
+#         continue
+#     print(f"{n} * {i} = {n*i}")
+
+# python program to using pass 
+# marks = (22,24,21,19,20,24)
+# for m in marks:
+#     pass
+# i = 5   
+# while(i < 10):
+#     print(i)
+#     i += 1
+# i = 0
+# while(i < 5):
+#     print("*")
+#     i += 1
+
+# n = 3
+# for i in range(n):
+#     for j in range(n):
+#      print("*" ,end="")
+#     print()
+
+# n = 3
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(j,end=" ")
+#     print()
+
+# n = 3
+# for i in range(n,n+1):
+#     for j in range(i):
+#         print("*")
+#     print()
+
+# n = 3
+# for i in range(n,n+1):
+#     for j in range(i):
+#         print("*",end=" ")
+#     print()
+
+# n = 3
+# for i in range(1,n+1):
+#     for j in range(i):
+#         print("*",end=" ")
+#     print()
+
+# n = 4
+# for i in range(1,n):
+#     for j in range(1,i+1):
+#         print(j,end=" ")
+#     print()
+ 
+# n = 3
+# for i in range(n):
+#     for j in range(n):
+#         print("*",end=" ")
+#     print()
+
+n = 3
+for i in range(1,n+1):
+    for j in range(i):
+        print(i,end=" ")
+    print()
---
+++ Chapter7/while_loop.py
@@ -0,0 +1,45 @@
+# mylist = [2,56,3.34,"Shahan ",'A'];
+# i = 0; 
+# while(i < len(mylist)):
+#     print(mylist[i]);
+#     i += 1;
+
+# i = 0;
+# while(i <=100 & i % 5 == 0):
+#     print(i);
+#     i += 5;
+
+# for(int i = 0; i < =100 ; i++){
+#    print(i);
+# }
+
+# for i in range(0,101,10):
+#     print(i);
+
+# for i in range(1,11):
+#     print(i);
+# else:
+#     print("Program terminated succcessfully");
+
+#1
+# for i in range(1,11):
+#     print(i);
+
+# #2
+# for i in range(0,50,2):
+#     print(i)
+
+# num = int(input());
+# for i in (1,11):
+#     ans = i * num;
+#     print({i} ," *", {num}," = "  ,ans )
+
+# i = 0
+# while(i < 11):
+#     print("Shahan Waheed")
+#     i+=1
+
+i = 1
+while(i < 15):
+    print(i)
+    i += 1
