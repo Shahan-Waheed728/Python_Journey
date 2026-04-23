2b1419e324ae6d33de610c7785f8686c9a6085eb
Shahan-Waheed728 <shahanwaheed43@gmail.com>
2026-04-23 10:05:39 +0500

    Chapter1 added

---
+++ Chapter1/first.py
@@ -0,0 +1,4 @@
+print("Welcome in the world of Python!")
+print("Hi, Shahan Waheed ! Yes you have the ability to face challenges and achieve what you want in next 90 days.")
+print("Today is the first day  and i successfully write the first code in pyton.");
+print("CONGRATULATIONs 💪")
